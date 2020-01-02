a284480fa40752e78d29b55579d7f50a319eff8d
ClintonEnyinna <enyinnaclinton@gmail.com>
2020-01-01 20:39:18 -0800

    Add bubble sort by str length functionality

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -17,3 +17,23 @@ end
 
 sorted_arr = bubble_sort([2, 3, 5, 0])
 print sorted_arr
+print "\n"
+
+def bubble_sort_by(arr)
+  return "Error no block given" unless block_given?
+  counter = arr.length - 1
+  while counter.positive?
+    (0...counter).each do |i|
+      next unless yield(arr[i], arr[i + 1]).positive?
+
+      first_item = arr[i]
+      arr[i] = arr[i + 1]
+      arr[i + 1] = first_item
+    end
+    counter -= 1
+  end
+  arr
+end
+
+sorted_arr_of_str = bubble_sort_by(["hi", "hello", "hey"]) { |left, right| left.length - right.length }
+print sorted_arr_of_str
